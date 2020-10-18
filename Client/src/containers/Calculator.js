e26a8c1b84b624b763aa77c941d527fa5f1ec19e
Despoina <despoinasifouna@gmail.com>
2020-10-18 21:53:35 +0100

    add category and catg-dependent food inputs in calculator form

--- Client/src/containers/Calculator.js
+++ Client/src/containers/Calculator.js
@@ -4,29 +4,50 @@ import { connect } from 'react-redux';
 class Calculator extends Component {
     state = {
         userid: this.props.userid,
-        allTheFoods:[]
+        foodCategories:[],
+        categoryChosen: "",
+        foodsForCategory: []
     }
     componentDidMount() {
-        const getAllFoods = () => {
-            const URL = "http://localhost:5000"
-            fetch(`${URL}/api/foods/all`)
+        const URL = "http://localhost:5000"
+        const findCategories = () => {
+            fetch(`${URL}/api/foods/categories`)
             .then(resp => resp.json())
-            .then(resp => this.setState({allTheFoods: resp}))
+            .then(resp => this.setState({foodCategories: resp}))
             .catch(err=>console.log(err))
         }
-        getAllFoods();
+        findCategories();
+    }
+    componentDidUpdate() {
+        const URL = "http://localhost:5000"
+        const findFoodsByCategory = () => {
+            fetch(`${URL}/api/foods/categories/${this.state.categoryChosen}`)
+            .then(resp=> resp.json())
+            .then(resp => this.setState({foodsForCategory: resp}))
+            .catch(err=>console.log(err))
+        }
+        findFoodsByCategory();
     }
 
+    handleCategoryInput = e => {
+        this.setState({categoryChosen: e.target.value})
+    }
+    
     render() {
         return (
             <div>
                 <h1>User: {this.state.userid}</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="food">Food</label><br/>
-                      <input type="search" list="all-foods" name="food" onChange={this.handleInput} required></input><br/>
-                      <datalist id="all-foods">
-        {this.state.allTheFoods.map((item,idx) => (<option key={idx}>{item.foodname}</option>))}
-                      </datalist>
+                <label htmlFor="category">Category</label><br/>
+                    <input type="search" list="all-categories" name="category" onChange={this.handleCategoryInput} required></input><br/>
+                    <datalist id="all-categories">
+    {this.state.foodCategories.map((item,idx) => (<option key={idx}>{item.category}</option>))}
+                    </datalist>
+                <label htmlFor="food">Food</label><br/>
+                    <input type="search" list="all-foods" name="food" required></input><br/>
+                    <datalist id="all-foods">
+    {this.state.foodsForCategory.map((item,idx) => (<option key={idx}>{item.foodname}</option>))}
+                    </datalist>
                 </form>
             </div>
         )
