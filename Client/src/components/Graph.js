57816c5f091a4e82a7b8f872199c0074bb7d19f9
Despoina <despoinasifouna@gmail.com>
2020-10-21 21:02:45 +0100

    display graph with correct user data

--- Client/src/components/Graph.js
+++ Client/src/components/Graph.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import CanvasJSReact from '../assets/canvasjs.react';
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class Graph extends Component {
-    state = {
-        sumData: []
+    constructor() {
+        super();
+        this.state = {sumData: []};
+        this.toggleDataSeries = this.toggleDataSeries.bind(this);
     }
-
+    
     componentDidMount() {
         const URL = "http://localhost:5000"
         fetch(`${URL}/api/${this.props.userid}/allemissions`)
@@ -14,16 +19,80 @@ class Graph extends Component {
         .catch(err=>console.log(err))   
     }
 
+    toggleDataSeries(e){
+		if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+			e.dataSeries.visible = false;
+		}
+		else{
+			e.dataSeries.visible = true;
+		}
+		this.chart.render();
+	}
+
     render() {
-        const dates=[];
+        // const dates=[];
+        // const emissions = [];
+        // const water = [];
+        const emissionsDataPoints = [];
+        const waterWasteDataPoints = [];
         if (this.state.sumData.length > 0) {
             this.state.sumData.map((obj, index) => {
-                dates[index] = obj["date"]
+                emissionsDataPoints[index]=({x: new Date(obj["date"].slice(0,10)), y: parseFloat(obj["emissions_sum"])})
+                waterWasteDataPoints[index]=({x: new Date(obj["date"].slice(0,10)), y: parseFloat(obj["water_sum"])})
+                // dates[index] = obj["date"].slice(0,10);
+                // emissions[index]= obj["emissions_sum"].toFixed(2);
+                // water[index] = obj["water_sum"].toFixed(2);
             })
         } 
+        const options = {
+			theme: "light2",
+			animationEnabled: true,
+			title:{
+				text: "Your foodprint over time"
+			},
+			axisY: {
+				title: "CO2 emissions (CO2 kg-equivalents)",
+				titleFontColor: "#6D78AD",
+				lineColor: "#6D78AD",
+				labelFontColor: "#6D78AD",
+				tickColor: "#6D78AD"
+			},
+			axisY2: {
+				title: "Water waste (L)",
+				titleFontColor: "#51CDA0",
+				lineColor: "#51CDA0",
+				labelFontColor: "#51CDA0",
+				tickColor: "#51CDA0"
+			},
+			toolTip: {
+				shared: true
+			},
+			legend: {
+				cursor: "pointer",
+				itemclick: this.toggleDataSeries
+			},
+			data: [{
+				type: "spline",
+				name: "CO2 emissions",
+				showInLegend: true,
+				xValueFormatString: "DDD",
+				yValueFormatString: "####.##",
+				dataPoints: emissionsDataPoints
+			},
+			{
+				type: "spline",
+				name: "Fresh water waste",
+				axisYType: "secondary",
+				showInLegend: true,
+				xValueFormatString: "DDD",
+				yValueFormatString: "#####.##",
+				dataPoints: waterWasteDataPoints
+			}]
+		}
         return (
             <div>
-               <p>{dates}</p> 
+                {console.log(emissionsDataPoints)}
+               <CanvasJSChart options = {options} onRef={ref => this.chart = ref}/>
             </div>
         )
     }
